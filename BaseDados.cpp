b00a2426e451e4b98570629d66c8f64e9c6a269e
Valkajjik <lcsferreiradesousa@hotmail.com>
2022-01-18 14:09:20 +0000

    Initial commit

---
+++ BaseDados.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lucas on 18/01/22.
+//
+
+#include "BaseDados.h"
---
+++ BaseDados.h
@@ -0,0 +1,14 @@
+//
+// Created by lucas on 18/01/22.
+//
+
+#ifndef PROJETO2_BASEDADOS_H
+#define PROJETO2_BASEDADOS_H
+
+
+class BaseDados {
+
+};
+
+
+#endif //PROJETO2_BASEDADOS_H
